039a0c8adf68ad62c05deeecd670a7a4a6e4bb2e
Kopacz Thinkpad <kopaczjm@gmail.com>
2024-02-13 21:26:02 -0600

    Added fake DB model, admin controller and views.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AppsDevCoffee.Models;
+
+
+namespace AppsDevCoffee.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UserList()
+        {
+            List<User> users = DB.getUsers();
+            return View(users);
+        }
+
+        public IActionResult Orders()
+        {
+            return View();
+        }
+
+        public IActionResult Inventory()
+        {
+            return View();
+        }
+
+    }
+
+}
---
+++ Models/DB.cs
@@ -0,0 +1,23 @@
+﻿namespace AppsDevCoffee.Models
+{
+    public static class DB
+    {
+
+        public static List<User> userList = [
+
+            new User("Joe", "Kopacz", "Joe@mail.com", "Password1!"),
+            new User("Angie", "Kopacz", "Angie@mail.com", "Password1!"),
+            new User("Sebastian", "Kopacz", "Seb@mail.com", "Password1!"),
+            new User("Lewis", "Kopacz", "Lewis@mail.com", "Password1!")
+
+            ];
+
+        public static List<User> getUsers()
+        {
+            List<User> users = [];
+            users = userList;
+
+            return users;
+        }
+    }
+}
